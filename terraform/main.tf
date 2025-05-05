1f0cfa415c6e52fa7a1565ac39fa3fd5d051a471
ian <ianwong2111@gmail.com>
2025-05-05 20:28:34 +0800

    Added security module

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,4 +7,18 @@ module "network" {
   private_subnets = var.private_subnets
   db_subnets     = var.db_subnets
   nat_ami        = var.nat_ami
+  nat_security_group_id = module.security.nat_security_group_id
+
 }
+
+module "security" {
+  source = "./security"
+  
+  project_name   = var.project_name
+  vpc_id = module.network.vpc_id
+  vpc_cidr = var.vpc_cidr
+}
+
+
+
+
