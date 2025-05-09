91344211a95aaf981ed2a0dd7d3f5cf38db47073
ian <ianwong2111@gmail.com>
2025-05-10 00:05:41 +0800

    Create bastion instance and generated private key pairs for ssh

---
+++ terraform/compute/bastion.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "bastion" {
+    subnet_id = "${var.subnet_ids["public_az1"]}"
+    instance_type = "t2.micro"
+    ami = var.ami_id
+    associate_public_ip_address = true
+    vpc_security_group_ids = [var.bastion_security_group_id]
+    key_name = aws_key_pair.bastion_key.key_name
+
+    tags = {
+        Name = "${var.project_name}-bastion"
+        Tier = "public-a"
+    }
+}   
+
+# Generate a SSH key pair for bastion host
+resource "aws_key_pair" "bastion_key" {
+    key_name = "${var.project_name}-bastion-key"
+    public_key = tls_private_key.bastion_key.public_key_openssh
+}
+
+# Generate public and private key 
+resource "tls_private_key" "bastion_key" {
+    algorithm = "ED25519"
+}
+
+resource "local_file" "private_key" {
+    content = tls_private_key.bastion_key.private_key_pem
+    filename = "${path.module}/bastion_key.pem"
+}
