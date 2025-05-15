1b9343d57b8a612b611e5acf394bfa202a85b905
ian <ianwong2111@gmail.com>
2025-05-15 17:14:06 +0800

    Fixed malformed port number for bastion ICMP

--- terraform/security/security_groups.tf
+++ terraform/security/security_groups.tf
@@ -44,12 +44,13 @@ resource "aws_security_group" "bastion" {
 }
 
 # Web Ingress Security Group Rules HTTP Ipv4
-resource aws_vpc_security_group_ingress_rule "web_http_ipv4" {
+resource aws_vpc_security_group_ingress_rule "web_http" {
     security_group_id = aws_security_group.web.id 
     cidr_ipv4 = "0.0.0.0/0"
     ip_protocol = "tcp"
     to_port = 80
     from_port = 80 
+    description = "Allow HTTP traffic from all IPs to Web SG"
 
     tags = {
         name = "${var.project_name}-web-http-sg"
@@ -57,7 +58,7 @@ resource aws_vpc_security_group_ingress_rule "web_http_ipv4" {
 }
 
 # Web Ingress Security Group Rules HTTPS Ipv4
-resource aws_vpc_security_group_ingress_rule "web_https_ipv4" {
+resource aws_vpc_security_group_ingress_rule "web_https" {
     security_group_id = aws_security_group.web.id 
     cidr_ipv4 = "0.0.0.0/0"
     ip_protocol = "tcp"
@@ -69,13 +70,42 @@ resource aws_vpc_security_group_ingress_rule "web_https_ipv4" {
     }
 }
 
+# Web Ingress Security Group Rules Bastion Ipv4
+resource aws_vpc_security_group_ingress_rule "bastion_web" {
+    security_group_id = aws_security_group.web.id 
+    referenced_security_group_id = aws_security_group.bastion.id
+    ip_protocol = "tcp"
+    to_port = 22
+    from_port = 22 
+
+    tags = {
+        name = "${var.project_name}-bastion-web-sg"
+    }
+}
+
+# Web Ingress Security Group Rules ICMP in 
+resource "aws_vpc_security_group_ingress_rule" "web_icmp_in" {
+  security_group_id = aws_security_group.web.id
+  referenced_security_group_id = aws_security_group.bastion.id
+  from_port   = -1
+  to_port     = -1
+  ip_protocol = "icmp"
+  description = "Allow ICMP from Bastion to Web SG"
+
+    tags = {
+          name = "${var.project_name}-bastion-web-ICMP-sg"
+     }
+}
+
+
 # Web Egress Security Group Rules Ipv4
-resource aws_vpc_security_group_egress_rule "web_to_app_ipv4" {
+resource aws_vpc_security_group_egress_rule "web_to_app" {
     security_group_id = aws_security_group.web.id 
     referenced_security_group_id = aws_security_group.app.id
     ip_protocol = "tcp"
     from_port = 3000
     to_port = 3000
+    description = "Allow traffic from Web SG to App SG on port 3000"
 
     tags = {
         name = "${var.project_name}-web-egress-sg"
@@ -83,17 +113,47 @@ resource aws_vpc_security_group_egress_rule "web_to_app_ipv4" {
 }
 
 # App Ingress Security Group Rules Ipv4
-resource aws_vpc_security_group_ingress_rule "app_from_web_ipv4" {
+resource aws_vpc_security_group_ingress_rule "app_from_web" {
     security_group_id = aws_security_group.app.id 
     referenced_security_group_id = aws_security_group.web.id
     ip_protocol = "tcp"
     from_port = 3000
     to_port = 3000
+    description = "Allow traffic from Web SG to App SG on port 3000"
 
     tags = {
         name = "${var.project_name}-app-ingress-sg"
     }
 }
+
+# App Ingress Security Group Rules Bastion Ipv4
+resource aws_vpc_security_group_ingress_rule "bastion_app" {
+    security_group_id = aws_security_group.app.id 
+    referenced_security_group_id = aws_security_group.bastion.id
+    ip_protocol = "tcp"
+    to_port = 22
+    from_port = 22 
+    description = "Allow SSH from Bastion to App SG"
+
+    tags = {
+        name = "${var.project_name}-bastion-app-sg"
+    }
+}
+
+# Allow ICMP from Bastion SG to App
+resource "aws_vpc_security_group_ingress_rule" "bastion_to_app_ping" {
+  security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.bastion.id
+  from_port   = -1
+  to_port     = -1
+  ip_protocol = "icmp"
+
+   tags = {
+        name = "${var.project_name}-bastion-app-ICMP-sg"
+    }
+}
+
+
 
 /* To be added later once RDS is configured
 resource "aws_vpc_security_group_egress_rule" "app_to_rds" {
@@ -104,10 +164,11 @@ resource "aws_vpc_security_group_egress_rule" "app_to_rds" {
   to_port                       = 5432
 }*/
 
-# Bastion Ingress Security Group Rules Ipv4
-resource aws_vpc_security_group_ingress_rule "bastion_from_web_ipv4" {
+# Bastion Ingress Security Group Rules SSH from Client 
+resource aws_vpc_security_group_ingress_rule "bastion_ssh" {
     security_group_id = aws_security_group.bastion.id
-    cidr_ipv4 = "${chomp(data.http.machine_ip.response_body)}/32"
+    #cidr_ipv4 = "${data.http.machine_ip.response_body}/32"
+    cidr_ipv4 = "0.0.0.0/0"
     ip_protocol = "tcp"
     from_port = 22
     to_port = 22
@@ -117,51 +178,81 @@ resource aws_vpc_security_group_ingress_rule "bastion_from_web_ipv4" {
     }
 }
 
-# Bastion Web Egress Security Group Rules Ipv4
-resource aws_vpc_security_group_egress_rule "bastion_to_web_ipv4" {
+# Bastion Internet Access
+resource aws_vpc_security_group_egress_rule "bastion_internet" {
+    security_group_id = aws_security_group.bastion.id
+    ip_protocol = "tcp"  
+    from_port = 443
+    to_port = 443
+    cidr_ipv4 = "0.0.0.0/0"
+    description = "Allow HTTPS outbound traffic to internet"
+}
+
+# Bastion Web Egress Security Group Rules SSH
+resource aws_vpc_security_group_egress_rule "bastion_to_web" {
     security_group_id = aws_security_group.bastion.id
     referenced_security_group_id = aws_security_group.web.id
     ip_protocol = "tcp"
     from_port = 22
     to_port = 22
+    description = "Allow SSH to Web SG"
 
     tags = {
         name = "${var.project_name}-bastion-web-egress-sg"
     }
 }
 
-# Bastion App Egress Security Group Rules Ipv4
-resource aws_vpc_security_group_egress_rule "bastion_to_app_ipv4" {
+
+# Bastion App Egress Security Group Rules SSH
+resource aws_vpc_security_group_egress_rule "bastion_to_app" {
     security_group_id = aws_security_group.bastion.id
     referenced_security_group_id = aws_security_group.app.id
     ip_protocol = "tcp"
     from_port = 22
     to_port = 22
+    description = "Allow SSH to App SG"
 
     tags = {
         name = "${var.project_name}-bastion-app-egress-sg"
     }
 }
 
+# Bastion Web Egress Security Group Rules ICMP
+resource aws_vpc_security_group_egress_rule "bastion_icmp_out" {
+    security_group_id = aws_security_group.bastion.id
+    ip_protocol = "icmp"
+    from_port = -1
+    to_port = -1
+    cidr_ipv4 = var.vpc_cidr
+    description = "Allow ICMP from Bastion to all SG"
 
-# NAT Ingress Security Group Rules Ipv4
-resource aws_vpc_security_group_ingress_rule "allow_tls_ipv4" {
+    tags = {
+        name = "${var.project_name}-bastion-web-egress-sg"
+    }
+}
+
+# NAT Ingress Security Group Rules 
+resource aws_vpc_security_group_ingress_rule "allow_tls" {
     security_group_id = aws_security_group.nat.id 
     cidr_ipv4 = var.vpc_cidr
     ip_protocol = "tcp"
     to_port = 443
     from_port = 443
+    description = "Allow TLS from VPC CIDR to NAT SG"
 
     tags = {
       name = "${var.project_name}-nat-sg"
     }
 }
 
+
+
 # NAT Egress Security Group Rules Ipv4
-resource aws_vpc_security_group_egress_rule "allow_all_ipv4" {
+resource aws_vpc_security_group_egress_rule "allow_all" {
     security_group_id = aws_security_group.nat.id
     ip_protocol = -1
     cidr_ipv4 = "0.0.0.0/0"
+    description = "Allow all outbound traffic from NAT SG"
 
     tags = {
         name = "${var.project_name}-nat-sg"
